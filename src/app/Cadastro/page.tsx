b7684e41d8427f7bfb0d39103f338b2225872b01
Larissa2801 <110750076+Larissa2801@users.noreply.github.com>
2025-09-01 21:58:04 -0300

    Ajuste design cadastro da fazena versão 1 vou alterar conforme o commit iniciall da Maria

--- src/app/Cadastro/page.tsx
+++ src/app/Cadastro/page.tsx
@@ -3,9 +3,14 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import Link from "next/link";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Slider } from "@/components/ui/slider";
+
 import {
   Card,
   CardHeader,
@@ -24,8 +29,12 @@ import {
 } from "@/components/ui/form";
 import { Checkbox } from "@/components/ui/checkbox";
 
-// ✅ Substituindo o toast do shadcn pelo Sonner
-import { toast } from "sonner";
+import { Tsukimi_Rounded } from "next/font/google";
+
+const tsukimi = Tsukimi_Rounded({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+});
 
 const schema = z
   .object({
@@ -36,6 +45,10 @@ const schema = z
     terms: z.boolean().refine((v) => v, {
       message: "Aceite os termos de uso",
     }),
+    farmName: z.string().min(3, "Informe o nome da fazenda"),
+    address: z.string().min(5, "Informe o endereço"),
+    city: z.string().min(2, "Informe a cidade"),
+    size: z.number().min(1, "Informe o porte da fazenda"),
   })
   .refine((data) => data.password === data.confirm, {
     path: ["confirm"],
@@ -43,20 +56,6 @@ const schema = z
   });
 
 type FormValues = z.infer<typeof schema>;
-
-//Importando a fonte que está no FIGMA, pro card title
-import { Tsukimi_Rounded } from "next/font/google";
-
-const tsukimi = Tsukimi_Rounded({
-  subsets: ["latin"],
- weight: ["300", "400", "600"], // inclui leve, normal e semibold
-});
-
-//Importando a seta pra esquerda de "Voltar"
-import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
-
-
 
 export default function CadastroPage() {
   const form = useForm<FormValues>({
@@ -67,60 +66,67 @@ export default function CadastroPage() {
       password: "",
       confirm: "",
       terms: false,
+      farmName: "",
+      address: "",
+      city: "",
+      size: 1,
     },
     mode: "onTouched",
   });
 
   function onSubmit(values: FormValues) {
-    // Somente front: simulando envio
     console.log("Payload (front-only):", values);
-
-    // ✅ Sonner toast
     toast.success("Cadastro enviado com sucesso!");
   }
 
-
-
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-200 dark:bg-gray-800 transition-colors duration-500">
-      <div className="relative flex w-[900px] h-[500px] rounded-lg overflow-hidden shadow-lg">
-
-        {/* Coluna da seta*/}
-        <div className="flex w-12 items-start justify-center bg-white">
-          <Link
-            href="/"
-            className="mt-6 ml-4 text-black hover:text-red-900 transition-colors"
-          >
-            <ArrowLeft className="w-7 h-7" strokeWidth={1} />
-          </Link>
-        </div>
-
-        {/* Coluna esquerda */}
-        <div className="flex items-center justify-center w-1/2 bg-white  ">
-
-          <Card className="w-full max-w-md border-0 shadow-none py-10 ">
-            <CardHeader>
-              <CardTitle className={`${tsukimi.className} text-3xl font-semibold text-red-900`}>Cadastro</CardTitle>
-              <CardDescription className="text-[12px] text-black">Preencha os campos para fazer seu cadastro na plataforma.</CardDescription>
-            </CardHeader>
-
-            <CardContent>
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex w-[1000px] h-[650px] rounded-lg overflow-hidden shadow-lg"
+        >
+          <div className="flex w-12 items-start justify-center bg-white">
+            <Link
+              href="/"
+              className="mt-6 ml-4 text-black hover:text-red-900 transition-colors"
+            >
+              <ArrowLeft className="w-7 h-7" strokeWidth={1} />
+            </Link>
+          </div>
+          <div className="flex items-center justify-center w-1/2 bg-white">
+            <Card className="w-full max-w-md border-0 shadow-none py-10">
+              <CardHeader>
+                <CardTitle
+                  className={`${tsukimi.className} text-3xl font-semibold text-red-900`}
+                >
+                  Cadastro
+                </CardTitle>
+                <CardDescription className="text-[12px] text-black">
+                  Preencha os campos para fazer seu cadastro na plataforma.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-6">
                   <FormField
                     control={form.control}
                     name="name"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel className="text-red-900 ">Nome completo</FormLabel>
+                        <FormLabel className="text-red-900 ">
+                          Nome completo
+                        </FormLabel>
                         <FormControl>
-                          <Input placeholder="Digite seu nome..." {...field} className=" border-red-900 focus-visible:ring-0 focus:border-red-90" />
+                          <Input
+                            placeholder="Digite seu nome..."
+                            {...field}
+                            className=" border-red-900 focus-visible:ring-0 focus:border-red-90"
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
-
                   <FormField
                     control={form.control}
                     name="email"
@@ -128,13 +134,17 @@ export default function CadastroPage() {
                       <FormItem>
                         <FormLabel className="text-red-900 ">E-mail</FormLabel>
                         <FormControl>
-                          <Input type="email" placeholder="Digite seu e-mail..." {...field} className=" border-red-900 focus-visible:ring-0 focus:border-red-90" />
+                          <Input
+                            type="email"
+                            placeholder="Digite seu e-mail..."
+                            {...field}
+                            className=" border-red-900 focus-visible:ring-0 focus:border-red-90"
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
-
                   <FormField
                     control={form.control}
                     name="password"
@@ -142,34 +152,48 @@ export default function CadastroPage() {
                       <FormItem>
                         <FormLabel className="text-red-900 ">Senha</FormLabel>
                         <FormControl>
-                          <Input type="password" placeholder="Digite sua senha..." {...field} className=" border-red-900 focus-visible:ring-0 focus:border-red-90" />
+                          <Input
+                            type="password"
+                            placeholder="Digite sua senha..."
+                            {...field}
+                            className=" border-red-900 focus-visible:ring-0 focus:border-red-90"
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
-
                   <FormField
                     control={form.control}
                     name="confirm"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel className="text-red-900">Confirmar senha</FormLabel>
+                        <FormLabel className="text-red-900">
+                          Confirmar senha
+                        </FormLabel>
                         <FormControl>
-                          <Input type="password" placeholder="Digite novamente sua senha..." {...field} className=" border-red-900 focus-visible:ring-0 focus:border-red-90" />
+                          <Input
+                            type="password"
+                            placeholder="Digite novamente sua senha..."
+                            {...field}
+                            className=" border-red-900 focus-visible:ring-0 focus:border-red-90"
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
-
                   <FormField
                     control={form.control}
                     name="terms"
                     render={({ field }) => (
                       <FormItem className="flex items-start gap-2">
                         <FormControl>
-                          <Checkbox checked={field.value} onCheckedChange={field.onChange} className=" border-red-900 focus-visible:ring-0 focus:border-red-90" />
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                            className=" border-red-900 focus-visible:ring-0 focus:border-red-90"
+                          />
                         </FormControl>
                         <div className="space-y-1 leading-none">
                           <FormLabel className="font-normal">
@@ -180,40 +204,109 @@ export default function CadastroPage() {
                       </FormItem>
                     )}
                   />
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="flex w-1/2 bg-white p-8 items-stretch">
+            <Card className="w-full h-full bg-red-900 text-white flex flex-col border-0 shadow-none">
+              <CardHeader>
+                <CardTitle
+                  className={`${tsukimi.className} text-2xl font-normal text-white items-center justify-center text-center`}
+                >
+                  Dados da Fazenda
+                </CardTitle>
+                <CardDescription className="text-[12px] !text-white/80 text-center">
+                  Preencha os campos com os dados da sua fazenda
+                </CardDescription>
+              </CardHeader>
 
+              <CardContent className="flex-1 space-y-4">
+                <FormField
+                  control={form.control}
+                  name="farmName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome da Fazenda</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite o nome da fazenda..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Endereço / Localidade</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite o endereço da fazenda..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Cidade</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Digite a cidade..." {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="size"
+                  render={({ field: { value, onChange } }) => (
+                    <FormItem>
+                      <FormLabel>Defina o porte</FormLabel>
+                      <FormControl>
+                        <div className="flex items-center gap-4">
+                          <span>Pequena</span>
+                          <Slider
+                            onValueChange={(val: number[]) => onChange(val[0])}
+                            defaultValue={[1]}
+                            max={3}
+                            step={1}
+                            className="w-full"
+                            trackClassName="bg-black"
+                            rangeClassName="bg-pink-300"
+                            thumbClassName="h-4 w-4 bg-pink-300 focus-visible:ring-pink-300"
+                          />
+                          <span>Grande</span>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
 
-                </form>
-              </Form>
-            </CardContent>
-
-            <CardFooter className="text-sm text-muted-foreground">
-              {/* Você pode colocar aqui um Link para /login depois */}
-            </CardFooter>
-          </Card>
-        </div>
-
-        {/* Coluna direita: estica e dá padding uniforme */}
-        <div className="flex w-1/2 bg-white p-8 items-stretch">
-          {/* Card vermelho esticando 100% da altura e largura */}
-          <Card className="w-full h-full bg-red-900 text-white flex flex-col border-0 shadow-none">
-            <CardHeader>
-              <CardTitle className={`${tsukimi.className} text-2xl font-normal text-white items-center justify-center text-center`}>Dados da Fazenda</CardTitle>
-              <CardDescription className="text-[12px] !text-white/80 text-center">
-                Preencha os campos com os dados da sua fazenda
-              </CardDescription>
-            </CardHeader>
-
-            {/* ... conteúdo da coluna direita ... */}
-            <Button
-              type="submit"
-              className="w-1/3 mx-auto border border-white text-white bg-transparent hover:bg-white hover:text-red-900 transition-colors"
-            >
-              Cadastrar
-            </Button>
-
-          </Card>
-        </div>
-      </div>
+              <CardFooter className="flex-col gap-2 p-6">
+                <Button
+                  type="submit"
+                  className="w-1/3 mx-auto border border-white text-white bg-red-900 hover:bg-red-800 transition-colors flex items-center justify-center"
+                >
+                  Cadastrar <ArrowRight className="ml-2 h-4 w-4" />
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+        </form>
+      </Form>
     </main>
   );
 }
